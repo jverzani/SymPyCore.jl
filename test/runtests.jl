4ff2431812e216ba5a33e524fd740dcb5630b19b
jverzani <jverzani@gmail.com>
2023-10-11 18:54:29 -0400

    clean up tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,5 @@
-if lowercase(get(ENV, "CI", "false")) == "true"
-    include("install_dependencies.jl")
-end
-
-using SymPy
 using Test
-import SymbolicUtils  # test loading extension
 
-include("tests.jl")
-include("test-math.jl")
-include("test-matrix.jl")
-include("test-logical.jl")
-#include("test-specialfuncs.jl")  ## XXX NEEDS WORK
-#include("test-permutations.jl")
-include("test-physics.jl")
-include("test-external-module.jl")
-include("test-latexify.jl")
-include("test-ode.jl")
+@testset "Tests are done in glue packages" begin
+    @test 2 + 2 == 4
+end
